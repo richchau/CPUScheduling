52504006ce7b66e308c9b231b5c5524d0ee9ea14
richsensations <32314978+richsensations@users.noreply.github.com>
2018-09-14 17:39:32 -0700

    Added turn around computation to FCFS.

--- ProcessScheduling/src/FCFS.java
+++ ProcessScheduling/src/FCFS.java
@@ -1,3 +1,5 @@
+import java.awt.geom.FlatteningPathIterator;
+
 /*
  * 
  * First Come First Serve Algorithm
@@ -6,6 +8,7 @@
 public class FCFS {
 	
 	private Process[] processArr;
+	private float[] waitingTimesArr;
 	
 	public FCFS(Process[] procArr){
 		processArr = procArr;
@@ -15,28 +18,40 @@ public class FCFS {
 	public void computeAvgWaitingTime(){
 		float serviceTime;
 		float totWaitingTime = 0;
+		float waitingTime;
+		float totTurnAroundTime = 0;
 		
 		serviceTime = processArr[0].getArrivalTime();
+		totTurnAroundTime += processArr[0].getExpTotRunTime();
+		
 		for (int i = 0; i < processArr.length - 1; i++){
 			if ((processArr[i].getExpTotRunTime() + serviceTime) < processArr[i+1].getArrivalTime()){
-				totWaitingTime += processArr[i + 1].getArrivalTime() - (processArr[i].getExpTotRunTime() + serviceTime);
+				waitingTime = processArr[i + 1].getArrivalTime() - (processArr[i].getExpTotRunTime() + serviceTime);
+				totTurnAroundTime += (waitingTime + processArr[i + 1].getExpTotRunTime());
+				totWaitingTime += waitingTime;
 				serviceTime = processArr[i + 1].getArrivalTime();
+				
+				//Stops scheduling if process tries to start after 99 quantas
+				if (serviceTime > 99){
+					break;
+				}
 			}else{
-				totWaitingTime += ((processArr[i].getExpTotRunTime() + serviceTime) - processArr[i+1].getArrivalTime());
+				waitingTime = ((processArr[i].getExpTotRunTime() + serviceTime) - processArr[i+1].getArrivalTime());
+				totTurnAroundTime += (waitingTime + processArr[i + 1].getExpTotRunTime());
+				totWaitingTime += waitingTime;
 				serviceTime = processArr[i].getExpTotRunTime() + serviceTime;
+				
+				//Stops scheduling if process tries to start after 99 quantas
+				if (serviceTime > 99){
+					break;
+				}
 			}
 		}
 		
 		float avgWaitingTime = totWaitingTime / (processArr.length - 1);
+		float avgTotTurnAroundTime = totTurnAroundTime / (processArr.length);
 		System.out.println("FCFS Average Waiting Time: " + avgWaitingTime);
-		
-	}
-	
-	public void computeAvgTurnaroundTime(){
-		
-	}
-	
-	public void computeAvgResponseTime(){
+		System.out.println("FCFS Average Turnaround Time: " + avgTotTurnAroundTime);
 		
 	}
 	
--- ProcessScheduling/src/Scheduler.java
+++ ProcessScheduling/src/Scheduler.java
@@ -7,16 +7,28 @@ public class Scheduler {
 
 	public static void main(String[] args) {
 		ProcessGenerator processGenerator = new ProcessGenerator();
-		processGenerator.createProcess(40);
-		Process[] procArr = processGenerator.getProcessArray();
 		
-		//Test: prints out contents of process array (NAME, ARRIVALTIME, TOTALRUNTIME)
+		/*Test: prints out contents of process array (NAME, ARRIVALTIME, TOTALRUNTIME)
 		for(int i = 0; i < procArr.length; i++){
 			System.out.println(procArr[i].getProcName() + " " + procArr[i].getArrivalTime() + " " + procArr[i].getExpTotRunTime());
 		}
+		*/
 	
-		//FCFS Algorithm (just 1 run as of right now)
-		FCFS fcfs = new FCFS(procArr);
+		//FCFS Algorithm
+		System.out.println("FCFS Algorithm: " + "\n");
+		for (int i = 0; i < 5; i++){
+			System.out.println("Run #: " + (i+1));
+			processGenerator.createProcess(40);
+			Process[] procArr = processGenerator.getProcessArray();
+			
+			for(int j = 0; j < procArr.length; j++){
+				System.out.println(procArr[j].getProcName() + " " + procArr[j].getArrivalTime() + " " + procArr[j].getExpTotRunTime());
+			}
+			System.out.println();
+			FCFS fcfs = new FCFS(procArr);
+			System.out.println("\n" + "\n");
+		}
+	
 		
 		
 	}
