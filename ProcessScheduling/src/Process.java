cb12d8b121fab3dc6930cc08e07c9810a51a5581
Kevin Prakash <kevprakash@gmail.com>
2018-09-16 15:45:22 -0700

    Added remaingTime and completed variables and added a constructor

--- ProcessScheduling/src/Process.java
+++ ProcessScheduling/src/Process.java
@@ -3,7 +3,16 @@ public class Process {
 	private float arrivalTime;
 	private float expTotRunTime; 
 	private int priority; 
+	private float remainingTime;
+	private boolean completed = false;
 	private String name;
+	
+	public Process(float arrivalTime, float expectedTotalRunTime, int priority, String name) {
+		this.arrivalTime = arrivalTime;
+		expTotRunTime = expectedTotalRunTime;
+		this.priority = priority;
+		this.name = name;
+	}
 	
 	public float getArrivalTime(){
 		return arrivalTime;
@@ -35,6 +44,23 @@ public class Process {
 	
 	public void setProcName(String name){
 		this.name = name;
+	}
+	
+	public float getRemainingTime() {
+		return remainingTime;
+	}
+
+	public void reduceRemainingTime(float amount) {
+		if(remainingTime - amount <= 0) {
+			remainingTime = 0;
+			completed = true;
+		}else {
+			remainingTime = remainingTime - amount;
+		}
+	}
+
+	public boolean isCompleted() {
+		return completed;
 	}
 	
 	public String toString() {
