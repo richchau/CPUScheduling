250fdf1fe57611d622923d1fc1b4381b0aba5b57
jmercad0 <lakers722@gmail.com>
2018-09-20 23:17:02 -0700

    implemented addProcess & getNext
    added process now field

--- ProcessScheduling/src/RR.java
+++ ProcessScheduling/src/RR.java
@@ -1,35 +1,39 @@
 
+import java.util.LinkedList;
+
+
 public class RR  extends Scheduler{
-	private Process[] processArr;
+	private Process now;
+	private LinkedList<Process> processArr;
 	private float[] rem_bt;
 	private int quantum;
-	public RR(Process[] aProcessArr, int aQuantum) {
-		processArr = aProcessArr;
-		rem_bt = new float[processArr.length];
+	public RR(int aQuantum) {
+		processArr = new LinkedList<Process>();
 		quantum = aQuantum;
 	}
-	//
+	
 	public void computeAvgWaitingTime(){
 		//initialize burst time & remaining burst time array
-		float bt[] = new float[processArr.length];
-		for (int i = 0;i<processArr.length;i++) {
-			rem_bt[i] = processArr[i].getRemainingTime();
+		float bt[] = new float[processArr.size()];
+		for (int i = 0;i<processArr.size();i++) {
+			rem_bt[i] = processArr.pop().getRemainingTime();
 			bt[i] = rem_bt[i];
 		}
 		//initialize wait time array
-		float wt[] = new float[processArr.length];
-		//initialize time
+		float wt[] = new float[processArr.size()];
+		
+		float rt[] = new float[processArr.size()];
 		float t = 0;
 		//check if all processes finished
 		while(!processesDone()) {
 			// give time slice to each process in numerical order
-			for (int i = 0;i<processArr.length;i++) {
+			for (int i = 0;i<processArr.size();i++) {
 				//if remaining burst time for i'th process > quantum
 				if (rem_bt[i]>quantum) {
 					// add quantum to total time & subtract burst time by quantum
 					t = t+quantum;
 					rem_bt[i] -=quantum;
-				}
+				}// skip if burst time is depleted to 0
 				else if(rem_bt[i]==0) {
 					rem_bt[i]=rem_bt[i];	
 				}
@@ -44,8 +48,8 @@ public class RR  extends Scheduler{
 			}
 		}
 		//initialize turn around time array
-		float tat[] = new float[processArr.length];
-		for (int i = 0; i<processArr.length;i++) {
+		float tat[] = new float[processArr.size()];
+		for (int i = 0; i<processArr.size();i++) {
 			//turn around time = burst time + wait time of i'th process
 			tat[i] = bt[i]+wt[i];
 		}
@@ -55,15 +59,15 @@ public class RR  extends Scheduler{
 		System.out.println("Processes " + " Burst time " +
                 " Waiting time " + " Turn around time");
 		// calculate total waiting time and total turn around time
-		for (int i = 0; i<processArr.length;i++) {
+		for (int i = 0; i<processArr.size();i++) {
 			total_Wt = wt[i]+total_Wt;
 			total_Tat = tat[i]+total_Tat;
 			System.out.println(" " + (i+1) + "\t\t" + bt[i] +"\t " +
                     wt[i] +"\t\t " + tat[i]);
 		}
 		// compute and print avg waiting time & avg turn around time
-		System.out.println("Average waiting time = " + total_Wt / (float)processArr.length);
-		System.out.println("Average turn around time = " + total_Tat / (float)processArr.length);
+		System.out.println("Average waiting time = " + total_Wt / (float)processArr.size());
+		System.out.println("Average turn around time = " + total_Tat / (float)processArr.size());
 	}
 	public boolean processesDone() {
 		boolean truth = true;
@@ -74,14 +78,31 @@ public class RR  extends Scheduler{
 		}
 		return truth;
 	}
+	
+    
 	public Process getNext() {
 		// TODO Auto-generated method stub
-		return null;
+		Process p = now.getNext();
+		now = p;
+		return p;
 	}
 
 	@Override
 	public void addProcess(Process p) {
 		// TODO Auto-generated method stub
-		
+		if (processArr.size()==0) {
+			p.setNext(p);
+			now = p;
+			processArr.add(p);
+		}else if (processArr.size() == 1){
+			p.setNext(processArr.getFirst());
+			processArr.add(p);
+			processArr.getFirst().setNext(p);
+		}else {
+			p.setNext(processArr.getFirst());
+			processArr.add(p);
+			processArr.getLast().setNext(p);
+		}
 	}
+	
 }
